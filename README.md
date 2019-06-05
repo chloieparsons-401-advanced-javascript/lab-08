676cb22cb2655d8e199a57942a2a28efdc50ce0b
Chloie Parsons <chloieparsons@gmail.com>
2019-06-05 15:20:26 -0700

    Working on tests, added to categories, products, and products schema to help understand TDD

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # lab-08
 data-modeling
+
+Heroku: https://chloieparsons-401-advanced-jav.herokuapp.com/
--- src/models/products.js
+++ src/models/products.js
@@ -1,9 +1,6 @@
 'use strict';
 
-const uuid = require('uuid/v4');
-
-const schema = {
-};
+const schema = require('./products-schema.js');
 
 class Products {
 
@@ -11,19 +8,46 @@ class Products {
     this.database = [];
   }
 
-  get(id) {
+  get(_id) {
+    let queryObject = _id ? {_id} : {};
+    return schema.find(queryObject);
   }
   
+  getBeachByName(name){
+    let queryObject = {name};
+    return schema.find(queryObject);
+  }
   post(entry) {
+    let record = new schema(entry);
+    return record.save();
   }
 
-  put(id, entry) {
+  put(_id, record) {
+    return schema.findByIdAndUpdate(_id, record, {new:true});
   }
 
-  delete(id) {
+  delete(_id) {
+    return schema.findByIdAndDelete(_id);
   }
 
   sanitize(entry) {
+    let valid = true:
+    let record = {};
+
+    Object.keys(schema).forEach(beach => {
+      if(schema[beach].required){
+        if(entry[beach]) {
+          record[beach] = entry[beach];
+
+        } else {
+            valid = false;
+        }
+      } else {
+        record[beach] = entry[beach];
+      }
+    });
+
+    return valid ? record : undefined;
   }
 
 }
