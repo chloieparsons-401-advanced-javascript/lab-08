47ba5a06bd78080a02d8b397f12703a64762e51e
Chloie Parsons <chloieparsons@gmail.com>
2019-06-05 17:40:48 -0700

    Four tests passing for Products WOOHOO

--- __tests__/products.test.js
+++ __tests__/products.test.js
@@ -8,12 +8,42 @@ afterAll(supergoose.stopDB);
 
 describe('Products model', () => {
 
-  it('can post() a new beach', () => {
+  it('can post() a new beach object', () => {
     let obj = {beach_name: 'Aquasol Beach Park', airport_code: 'MBJ', resort_name: 'Theme Park'};
 
     return products.post(obj)
       .then(record => {
+          expect(obj.beach_name).toBeDefined();
           expect(obj.airport_code).toEqual(obj.airport_code.toUpperCase());
+          expect(obj.resort_name).toBeDefined();
         })
       });
+
+    it('can get() a beach object', () => {
+      let obj = {beach_name: 'Aquasol Beach Park', airport_code: 'MBJ', resort_name: 'Theme Park'};
+
+      return products.post(obj)
+        .then(record => {
+          return products.get(record.id)
+            .then(product => {
+              expect(obj.beach_name).toBeDefined();
+              expect(obj.airport_code).toEqual(obj.airport_code.toUpperCase());
+              expect(obj.resort_name).toBeDefined();
+          })
+        })
+    });
+
+    it('can put() a beach object', () => {
+      let obj = {beach_name: 'Aquasol Beach Park', airport_code: 'MBJ', resort_name: 'Secrets Wild Orchid Montego'};
+
+      let newObj = {beach_name: 'Private Beach', airport_code: 'MBJ', resort_name: 'Theme Park'};
+
+      return products.post(obj)
+        .then(record => {
+          return products.put(record.id, newObj)
+            .then(updatedProduct => {
+              expect(updatedProduct.beach_name).toEqual(newObj.beach_name);
+          })
+        })
+    });
 });
