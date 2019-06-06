2c384f80ec1c278c27af14efbeca7ff291cb29a5
Chloie Parsons <chloieparsons@gmail.com>
2019-06-06 08:49:03 -0700

    Fixing heroku error

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 const mongooseOptions = {
-  useNewUrlParser:true,
+  useNewUrlParser: true,
   useCreateIndex: true,
 };
 mongoose.connect(process.env.MONGODB_URI, mongooseOptions);
