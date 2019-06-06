55f33e0e544eb8a6dea85b0ca49c2178883c8bfa
Chloie Parsons <chloieparsons@gmail.com>
2019-06-05 17:01:25 -0700

    Working on product tests, first one passed

--- __tests__/products.test.js
+++ __tests__/products.test.js
@@ -1,8 +1,19 @@
 const Products = require('../src/models/products');
-const beaches = new Products();
+const products = new Products();
 
 const supergoose = require('./supergoose');
 
+beforeAll(supergoose.startDB);
+afterAll(supergoose.stopDB);
+
 describe('Products model', () => {
 
-})
+  it('can post() a new beach', () => {
+    let obj = {beach_name: 'Aquasol Beach Park', airport_code: 'MBJ', resort_name: 'Theme Park'};
+
+    return products.post(obj)
+      .then(record => {
+          expect(obj.airport_code).toEqual(obj.airport_code.toUpperCase());
+        })
+      });
+});
--- src/models/products-schema.js
+++ src/models/products-schema.js
@@ -4,8 +4,12 @@ const mongoose = require('mongoose');
 
 const beaches = mongoose.Schema({
   beach_name: {type:String, required:true},
-  airport_code: {type:String, required:true, uppercase:true,enum: ['OCI', 'MBJ', 'KIN']},
+  airport_code: {type:String, required:true, uppercase:true, enum: ['OCI', 'MBJ', 'KIN']},
   resort_name: {type:String, required:true},
+});
+
+beaches.pre('save', function() {
+  this.airport_code = this.airport_code.toUpperCase();
 });
 
 module.exports = mongoose.model('beaches', beaches);
